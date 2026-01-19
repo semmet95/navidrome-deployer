7a9c4747473c06c6dea3ffd14dffcf067f95897d
Amit Singh <singhamitch@outlook.com>
2026-01-19 16:57:15 +0530

    chore: adds k3s cluster and longhorn preflight setups
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,20 +7,13 @@ sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl create ns longhorn-system
 
-# Longhorn installation
+# Longhorn setup
 rm -rf /usr/local/bin/longhornctl
 curl -L https://github.com/longhorn/cli/releases/download/v1.10.1/longhornctl-linux-amd64 -o longhornctl
 chmod +x longhornctl
 mv ./longhornctl /usr/local/bin/longhornctl
 longhornctl install preflight
 longhornctl check preflight
-kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.10.1/deploy/longhorn.yaml
-
-sleep 300
-kubectl get po -A
 
 # install mage
 go install github.com/magefile/mage@latest
-
-# K3S uninstallation
-# /usr/local/bin/k3s-uninstall.sh
