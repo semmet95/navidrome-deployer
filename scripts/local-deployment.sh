507778d50107313d8726423b6c8120db5790618a
Amit Singh <singhamitch@outlook.com>
2026-02-08 08:58:03 +0530

    chore: updates longhorn version (#27)
    * fix: updates script paths
    * chore: updates helm action version
    
    ---------
    
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/local-deployment.sh
+++ scripts/local-deployment.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ex
 
-source test-setup.sh
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+source "$SCRIPT_DIR/test-setup.sh"
+source "$SCRIPT_DIR/longhorn-preflight.sh"
 
 # helmfile setup
 if ! helm plugin list | grep -q "^diff[[:space:]]"; then
