732247181cc75faeca03c1b8bf1f5a4d8e83a415
Amit Singh <singhamitch@outlook.com>
2026-02-13 22:03:46 +0530

    chore: updates k3s config
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -11,7 +11,19 @@ else
     echo "Host OS not supported"
     exit 1
 fi
-sudo curl -sfL https://get.k3s.io | sh -s -
+sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="
+  server \
+  --kubelet-arg=v=1 \
+  --kube-controller-manager-arg=v=1 \
+  --kube-apiserver-arg=v=1 \
+  --write-kubeconfig-mode=644 \
+  --disable traefik \
+  --disable servicelb \
+  --disable metrics-server \
+  --disable-cloud-controller \
+  --disable coredns \
+  --node-name ci-k3s \
+" sh -s -
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
