286156c23b1405f3aeaca9915f6d2a7569a31cc1
Amit Singh <singhamitch@outlook.com>
2026-02-14 22:55:13 +0530

    chore: whitelists k3s in firewall service (#33)
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -4,12 +4,17 @@ set -ex
 # K3S setup
 if [ -f /etc/redhat-release ]; then
     sudo dnf install -y kernel-modules-extra
-    systemctl disable firewalld --now
+    sudo firewall-cmd --permanent --add-port=6443/tcp #apiserver
+    sudo firewall-cmd --permanent --zone=trusted --add-source=10.42.0.0/16 #pods
+    sudo firewall-cmd --permanent --zone=trusted --add-source=10.43.0.0/16 #services
+    sudo firewall-cmd --reload
 elif [ -f /etc/debian_version ]; then
     # the following changes are relevant for free tier GA runners
-    sudo ufw disable
     sudo apt-get update
     sudo apt-get install -y nfs-common
+    sudo ufw allow 6443/tcp #apiserver
+    sudo ufw allow from 10.42.0.0/16 to any #pods
+    sudo ufw allow from 10.43.0.0/16 to any #services
 else
     echo "Host OS not supported"
     exit 1
