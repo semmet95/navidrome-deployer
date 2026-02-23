2b75d733fcea51a35b2053fa159d0b1546c578e8
Amit Singh <singhamitch@outlook.com>
2026-02-23 21:34:19 +0530

    chore: updates cluster status logged messages
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/print-cluster-state.sh
+++ scripts/print-cluster-state.sh
@@ -5,4 +5,6 @@ kubectl get po -A -o wide
 kubectl describe deploy navidrome -n navidrome-system
 kubectl describe deploy filebrowser -n navidrome-system
 kubectl describe job filebrowser-reconfig -n navidrome-system
+kubectl get ClusterIssuer -A
+kubectl get Certificate -A
 kubectl get ing -A -o wide
