eb2280d8cf64c07f9ef280473843b6835bf8dd06
Amit Singh <singhamitch@outlook.com>
2026-03-03 15:42:03 +0530

    chore: reduces resource allocation for ga runner
    Signed-off-by: Amit Singh <singhamitch@outlook.com>

--- scripts/print-cluster-state.sh
+++ scripts/print-cluster-state.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-set -x
 
 kubectl get po -A -o wide
+echo '--------------------------------------------------'
 kubectl describe deploy navidrome -n navidrome-system
+echo '--------------------------------------------------'
 kubectl describe deploy filebrowser -n navidrome-system
+echo '--------------------------------------------------'
 kubectl describe job filebrowser-reconfig -n navidrome-system
+echo '--------------------------------------------------'
 kubectl get ClusterIssuer -A
+echo '--------------------------------------------------'
 kubectl get Certificate -A
+echo '--------------------------------------------------'
 kubectl get IngressRoute -A -o wide
+echo '--------------------------------------------------'
+kubectl logs -l app=filebrowser --all-containers=true --prefix=true -n navidrome-system
+echo '--------------------------------------------------'
+kubectl logs -l app=navidrome --all-containers=true --prefix=true -n navidrome-system
